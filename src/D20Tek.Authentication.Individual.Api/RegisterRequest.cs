338d2e6c89c6e996b132c47a45b051dbdd4e7ff9
Pedro Silva <pedrosi@microsoft.com>
2023-10-06 14:29:25 -0700

    Created basic solution for this project. Added existing auth projects for core, api, and client. Added build scripts ci and nuget release.

---
+++ src/D20Tek.Authentication.Individual.Api/RegisterRequest.cs
@@ -0,0 +1,15 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Minimal.Endpoints;
+using Microsoft.AspNetCore.Http;
+
+namespace D20Tek.Authentication.Individual.Api;
+
+internal sealed record RegisterRequest(
+    string UserName,
+    string GivenName,
+    string FamilyName,
+    string Email,
+    string Password,
+    string? PhoneNumber) : IRequest<IResult>;
