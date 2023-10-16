1fc47dafefe37cb56cf38788c9d4da0dda3149fe
Pedro Silva <pedrosi@microsoft.com>
2023-10-16 13:48:46 -0700

    Completed unit tests for AuthenticationService methods.

---
+++ tests/D20Tek.Authentication.Individual.Client.UnitTests/AuthenticationServiceTests.Password.cs
@@ -0,0 +1,147 @@
+﻿//---------------------------------------------------------------------------------------------------------------------
+// Copyright (c) d20Tek.  All rights reserved.
+//---------------------------------------------------------------------------------------------------------------------
+using D20Tek.Authentication.Individual.Client.Contracts;
+using System.Net;
+
+namespace D20Tek.Authentication.Individual.Client.UnitTests;
+
+public partial class AuthenticationServiceTests
+{
+    [TestMethod]
+    public async Task ChangePassword_WithValueRequest_ReturnsAuthResponse()
+    {
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAuthResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new ChangePasswordRequest
+        {
+            CurrentPassword = "old-password",
+            NewPassword = "new-password",
+            ConfirmPassword = "new-password"
+        };
+
+        // act
+        var result = await service.ChangePasswordAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task ChangePassword_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new ChangePasswordRequest
+        {
+            CurrentPassword = "old-password",
+            NewPassword = "new-password",
+            ConfirmPassword = "new-password"
+        };
+
+        // act
+        var result = await service.ChangePasswordAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+        result.ValueOrDefault.Should().BeNull();
+    }
+
+    [TestMethod]
+    public async Task GetPasswordResetToken_WithValueRequest_ReturnsAuthResponse()
+    {
+        // arrange
+        var serviceResponse = new ResetResponse("test-reset-token");
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new GetResetTokenRequest
+        {
+            Email = "tester@test.com"
+        };
+
+        // act
+        var result = await service.GetPasswordResetTokenAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task GetPasswordResetToken_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new GetResetTokenRequest
+        {
+            Email = "tester@test.com"
+        };
+
+        // act
+        var result = await service.GetPasswordResetTokenAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public async Task ResetPassword_WithValueRequest_ReturnsAuthResponse()
+    {
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAuthResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new ResetPasswordRequest
+        {
+            Email = "tester@test.com",
+            ResetToken = "test-reset-token",
+            NewPassword = "new-password",
+            ConfirmPassword = "new-password"
+        };
+
+        // act
+        var result = await service.ResetPasswordAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task ResetPassword_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new ResetPasswordRequest
+        {
+            Email = "tester@test.com",
+            ResetToken = "test-reset-token",
+            NewPassword = "new-password",
+            ConfirmPassword = "new-password"
+        };
+
+        // act
+        var result = await service.ResetPasswordAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+    }
+}
--- tests/D20Tek.Authentication.Individual.Client.UnitTests/AuthenticationServiceTests.cs
+++ tests/D20Tek.Authentication.Individual.Client.UnitTests/AuthenticationServiceTests.cs
@@ -2,32 +2,57 @@
 // Copyright (c) d20Tek.  All rights reserved.
 //---------------------------------------------------------------------------------------------------------------------
 using D20Tek.Authentication.Individual.Client.Contracts;
-using D20Tek.Minimal.Result.Client;
 using System.Net;
-using System.Net.Http.Json;
 
 namespace D20Tek.Authentication.Individual.Client.UnitTests;
 
 [TestClass]
-public class AuthenticationServiceTests
+public partial class AuthenticationServiceTests
 {
     [TestMethod]
-    public async Task ChangePassword_WithValueRequest_ReturnsAuthResponse()
+    public async Task DeleteAccount_WithValueRequest_ReturnsAccountResponse()
     {
         // arrange
-        var serviceResponse = AuthorizationFactory.CreateAuthResponse();
-        var httpClient = CreateHttpClientWithResponse(serviceResponse);
+        var serviceResponse = AuthorizationFactory.CreateAccountResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
 
         var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
-        var request = new ChangePasswordRequest
-        {
-            CurrentPassword = "old-password",
-            NewPassword = "new-password",
-            ConfirmPassword = "new-password"
-        };
 
         // act
-        var result = await service.ChangePasswordAsync(request);
+        var result = await service.DeleteAccountAsync();
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.UserId.Should().BeEquivalentTo(serviceResponse.UserId);
+    }
+
+    [TestMethod]
+    public async Task DeleteAccount_WithValueRequest_ReturnsAccountError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpClientWithProblem(HttpStatusCode.NotFound);
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+
+        // act
+        var result = await service.DeleteAccountAsync();
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public async Task GetAccount_WithValueRequest_ReturnsAccountResponse()
+    {
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAccountResponse();
+        var httpClient = HttpClientFactory.CreateHttpGetClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+
+        // act
+        var result = await service.GetAccountAsync();
 
         // assert
         result.Should().NotBeNull();
@@ -36,55 +61,165 @@ public class AuthenticationServiceTests
     }
 
     [TestMethod]
-    public async Task ChangePassword_WithValueRequest_ReturnsAuthError()
+    public async Task GetAccount_WithValueRequest_ReturnsAccountError()
     {
         // arrange
-        var httpClient = CreateHttpClientWithProblem(HttpStatusCode.NotFound);
-
+        var httpClient = HttpClientFactory.CreateHttpGetClientWithProblem(HttpStatusCode.NotFound);
         var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
-        var request = new ChangePasswordRequest
-        {
-            CurrentPassword = "old-password",
-            NewPassword = "new-password",
-            ConfirmPassword = "new-password"
-        };
 
         // act
-        var result = await service.ChangePasswordAsync(request);
+        var result = await service.GetAccountAsync();
 
         // assert
         result.Should().NotBeNull();
         result.IsSuccess.Should().BeFalse();
-        result.ValueOrDefault.Should().BeNull();
     }
 
-    private HttpClient CreateHttpClientWithResponse<T>(
-        T response,
-        HttpStatusCode statusCode = HttpStatusCode.OK)
+    [TestMethod]
+    public async Task Register_WithValueRequest_ReturnsAuthResponse()
     {
-        var message = new HttpResponseMessage(statusCode);
-        message.Content = JsonContent.Create(response);
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAuthResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
 
-        var httpClient = new Mock<HttpClient>();
-        httpClient.Setup(x => x.SendAsync(
-                    It.IsAny<HttpRequestMessage>(),
-                    It.IsAny<CancellationToken>()))
-                  .ReturnsAsync(message);
-
-        return httpClient.Object;
-    }
-
-    private HttpClient CreateHttpClientWithProblem(
-        HttpStatusCode statusCode = HttpStatusCode.InternalServerError)
-    {
-        var problem = new ProblemDetails
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new RegisterRequest
         {
-            Status = (int)statusCode,
-            Detail = "test error",
-            Title = "test-error",
-            Type = "test-type"
+            UserName = "TestUser",
+            GivenName = "Tester",
+            FamilyName = "McTest",
+            Email = "tester@test.com",
+            PhoneNumber = "555-555-5555",
+            Password = "Password-123!",
+            ConfirmPassword = "Password-123!"
         };
 
-        return CreateHttpClientWithResponse(problem, statusCode);
+        // act
+        var result = await service.RegisterAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task Register_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpGetClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new RegisterRequest
+        {
+            UserName = "TestUser",
+            GivenName = "Tester",
+            FamilyName = "McTest",
+            Email = "tester@test.com",
+            PhoneNumber = "555-555-5555",
+            Password = "Password-123!",
+            ConfirmPassword = "Password-123!"
+        };
+
+        // act
+        var result = await service.RegisterAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public async Task Login_WithValueRequest_ReturnsAuthResponse()
+    {
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAuthResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new LoginRequest
+        {
+            UserName = "TestUser",
+            Password = "Password-123!"
+        };
+
+        // act
+        var result = await service.LoginAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task Login_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpGetClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new LoginRequest
+        {
+            UserName = "TestUser",
+            Password = "Password-123!"
+        };
+
+        // act
+        var result = await service.LoginAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
+    }
+
+    [TestMethod]
+    public async Task Update_WithValueRequest_ReturnsAuthResponse()
+    {
+        // arrange
+        var serviceResponse = AuthorizationFactory.CreateAccountResponse();
+        var httpClient = HttpClientFactory.CreateHttpClientWithResponse(serviceResponse);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new UpdateProfileRequest
+        {
+            UserName = "TestUser",
+            GivenName = "Tester",
+            FamilyName = "McTest",
+            Email = "tester@test.com",
+            PhoneNumber = "555-555-5555"
+        };
+
+        // act
+        var result = await service.UpdateAccountAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeTrue();
+        result.Value.Should().BeEquivalentTo(serviceResponse);
+    }
+
+    [TestMethod]
+    public async Task Update_WithValueRequest_ReturnsAuthError()
+    {
+        // arrange
+        var httpClient = HttpClientFactory.CreateHttpGetClientWithProblem(HttpStatusCode.NotFound);
+
+        var service = AuthorizationFactory.CreateAuthenticationService(httpClient);
+        var request = new UpdateProfileRequest
+        {
+            UserName = "TestUser",
+            GivenName = "Tester",
+            FamilyName = "McTest",
+            Email = "tester@test.com",
+            PhoneNumber = "555-555-5555"
+        };
+
+        // act
+        var result = await service.UpdateAccountAsync(request);
+
+        // assert
+        result.Should().NotBeNull();
+        result.IsSuccess.Should().BeFalse();
     }
 }
